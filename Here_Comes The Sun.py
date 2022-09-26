840df29c9d1d8fdf58bb02830617bc2be912672d
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-09-26 19:21:29 -0400

    Add files via upload

---
+++ Here_Comes The Sun.py
@@ -0,0 +1,11 @@
+from microbit import  *
+
+while True:
+        display.show(Image(
+                "90909:"
+                "09990:"
+                "99999:"
+                "09990:"
+                "90909:"))
+
+      
